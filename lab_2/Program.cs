5254b0ef4a4357ada4b7ed7dbf8d7d809bc644a0
tesderfoy <98217566+tesderfoy@users.noreply.github.com>
2023-02-17 10:58:50 +0300

    Add files via upload

---
+++ lab_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность массива");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            int[] mas = new int[size];
+            Console.WriteLine("Введите элементы массива");
+            for ( int j = 0;j < size; j++)
+            {
+                mas[j] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for(int i = 0; i< mas.Length; i++)
+            {
+                int index = i;
+                int num = mas[i];
+
+                while(index > 0 && num<mas[index - 1])
+                {
+                    mas[index] = mas[index - 1];
+                    index--;
+                }
+                mas[index] = num;
+            }
+            Console.WriteLine("Отсортированный массив");
+            foreach (int a in mas)
+            {
+                Console.WriteLine(a);
+            }
+
+
+
+        }
+
+
+
+    }
+}
